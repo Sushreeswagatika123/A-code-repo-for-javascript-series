f23fb6b5e2fb9199ac880c4fffa271170f43f624
Sushreeswagatika123 <94607662+Sushreeswagatika123@users.noreply.github.com>
2026-06-05 05:24:52 +0000

    datatypes

---
+++ JAVASCRIPT/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+let name = "sushree"
+let age = 22
+
+/* datatypes
+
+//number=> 2 to the power of 52
+//bigint
+//string=>""
+//boolean=> true/false
+//null => standalone value
+//undefind =>
+//symbol=> unique
+
+
+//object
+
+*/
+
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof(undefined))
