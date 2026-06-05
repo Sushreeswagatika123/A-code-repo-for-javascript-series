acb23076f2c96ecc8e15cf474a667cbaf7591af6
Sushreeswagatika123 <94607662+Sushreeswagatika123@users.noreply.github.com>
2026-06-05 05:54:12 +0000

    conversion of datatypes

---
+++ JAVASCRIPT/conversion_operator.js
@@ -0,0 +1,24 @@
+let score = "22bgd"
+ //console.log(typeof score)
+ //console.log(typeof(score))
+
+ let valueNumber = Number(score)
+ //console.log(typeof valueNumber)
+ //console.log(valueNumber)
+
+
+ //"22" => number
+ //"22abc" => NaN
+ //"true" => 1, false => 0
+
+
+ let isLoggedIn = "Sushree"
+
+ let booleanisLoggedIn = Boolean(isLoggedIn)
+
+ console.log(booleanisLoggedIn)
+
+
+ //1 => true; 0 => false;
+ //""= > false
+ // "sushree" => true
