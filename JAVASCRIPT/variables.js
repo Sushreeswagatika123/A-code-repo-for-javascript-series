33ec6d19a12040fee807036141e37d9bb7ecb87d
Sushreeswagatika123 <94607662+Sushreeswagatika123@users.noreply.github.com>
2026-06-05 05:08:25 +0000

    variables

---
+++ JAVASCRIPT/variables.js
@@ -0,0 +1,16 @@
+const EmpId = 2010330
+var empMail = "sushreeswagatika@google.com"
+let empCity = "Binjharpur"
+empCountry = "India"
+
+
+empMail = "Jagannath44@google.com"
+empCity = "Puri"
+empCountry = "Japan"
+let empState;
+
+console.table([EmpId,empMail,empCity,empCountry,empState])
+
+/*
+prefer not to use var because it is scope based
+*/
