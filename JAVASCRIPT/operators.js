67643f8c794d02444d52b1ae2eebb39dcf9bfca6
Sushreeswagatika123 <94607662+Sushreeswagatika123@users.noreply.github.com>
2026-06-10 05:23:04 +0000

    operators

---
+++ JAVASCRIPT/operators.js
@@ -0,0 +1,26 @@
+let a = 7
+let b = 9
+
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a%b)
+
+let str1 = "hello"
+let str2 = " sushree"
+
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log(+true)
+
+// console.log(+"")
+
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null > 0)
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined > 0)
