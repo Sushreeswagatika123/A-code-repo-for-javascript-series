b0768c6701bbb4aa89cf7da3fe412406a2554f70
Sushreeswagatika123 <94607662+Sushreeswagatika123@users.noreply.github.com>
2026-06-10 05:37:55 +0000

    datatypes summary

---
+++ JAVASCRIPT/datatypes_summary.js
@@ -0,0 +1,19 @@
+//primitive
+
+// 7 types- string,number,boolean,nulll,undefined,symbol,BigInt
+
+//Reference - non primitive
+
+//Array,Objects,function
+
+const arr = ["shaktiman","nagraj","doga"]
+const myObj = {
+    name:"Sushree",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof(myFunction))
