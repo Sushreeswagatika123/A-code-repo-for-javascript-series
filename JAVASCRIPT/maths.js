60856264829f04b7275244fc9fc1651a6dc48e48
Sushreeswagatika123 <94607662+Sushreeswagatika123@users.noreply.github.com>
2026-06-16 14:57:44 +0000

    maths

---
+++ JAVASCRIPT/maths.js
@@ -0,0 +1,17 @@
+// console.log(Math)
+// console.log(Math.abs(-7))
+// console.log(Math.round(7.8))
+// console.log(Math.ceil(7.3))
+// console.log(Math.floor(7.9))
+// console.log(Math.min(5,3,8,6))
+// console.log(Math.max(5,3,8,6))
+
+
+// console.log(Math.random()) // given value betwwn 0 and 1
+// console.log(Math.random() * 10)
+// console.log((Math.random() * 10) +1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() *(max - min +1)) + min)
